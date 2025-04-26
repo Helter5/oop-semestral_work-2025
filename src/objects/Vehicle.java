693bf9e4224fbde66a545a1c427bdbaf529a90bf
Helter5 <rbull559@gmail.com>
2025-04-26 14:32:38 +0200

    Finished Vehicle.java

--- src/objects/Vehicle.java
+++ src/objects/Vehicle.java
@@ -5,14 +5,35 @@ public class Vehicle {
     final private int originalValue;
 
     public Vehicle(String licensePlate, int originalValue){
+        if (licensePlate == null) {
+            throw new IllegalArgumentException("License plate can't be null");
+        }
+
+        if (licensePlate.length() != 7) {
+            throw new IllegalArgumentException("Length of the license plate has to be 7");
+        }
+
+        for(int i = 0; i < licensePlate.length(); i++) {
+            char c = licensePlate.charAt(i);
+            if(!((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))) {
+                throw new IllegalArgumentException("License plate must contain only characters A-Z and numbers 0-9");
+            }
+        }
+
+        if (originalValue < 0) {
+            throw new IllegalArgumentException("Original value must be positive");
+        }
+
+        this.licensePlate = licensePlate;
+        this.originalValue = originalValue;
 
     }
 
     public String getLicensePlate(){
-        throw new UnsupportedOperationException("Not implemented yet"); // to do
+        return licensePlate;
     }
 
     public int getOriginalValue(){
-        throw new UnsupportedOperationException("Not implemented yet"); // to do
+        return originalValue;
     }
 }
